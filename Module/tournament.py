5256736a3c811cce6a060c535b8ae559e064e412
KSNATKIN <3522974@ppti-14-303-01.ufr-info-p6.jussieu.fr>
2019-02-04 19:33:00 +0100

    r

---
+++ Module/tournament.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 18:59:40 2019
+
+@author: 3522974
+"""
+
+
+from Simulation import Attaquant , Defenseur
+from soccersimulator import SoccerTeam
+
+def get_team ( nb_players ):
+    team = SoccerTeam ( name = " Philippe ’s ␣ Team " )
+    if nb_players == 1:
+        team . add ( " Striker " , Attaquant ())
+    if nb_players == 2:
+        team . add ( " Striker " , Attaquant ())
+        team . add ( " Blocker " , Defenseur ())
+    return team
+
+
+if __name__ == __main__:
+    from soccersimulator import Simulation , show_simu
+
+    # Check teams with 1 player and 2 players
+    team1 = get_team (1)
+    team2 = get_team (2)
+
+    # Create a match
+    simu = Simulation ( team1 , team2 )
+
+    # Simulate and display the match
+    show_simu ( simu )
