8eec133ecda32d967b9b53e98a282ffdd48ae93e
KSNATKIN <3522974@ppti-14-305-06.ufr-info-p6.jussieu.fr>
2019-04-08 19:14:07 +0200

    solo

---
+++ TMESOLO/examples/volleyball_question4.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 19:00:14 2019
+
+@author:  3522974
+"""
+
+from tools import *
+
+class Polyvalent (Strategy):
+    def __init__(self , force=2 , surface=25, zone=30, dribble=1.34):
+        Strategy.__init__(self, "Random")
+        self . force = force
+        self . surface = surface
+        self . zone = zone
+        self . dribble = dribble
+
+
+    def compute_strategy(self, state, id_team, id_player):
+        
+        s = SuperState ( state , id_team , id_player , self.dribble)
+        
+        if s.gotBall:
+            return s.shootOpti
+        else :
+            if (s.id_team == 1):
+                if (s.ball.x > GAME_WIDTH/2):
+                    return s.position_volley
+                else :
+                    return s.to_ball
+            else :
+                if (s.ball.x < GAME_WIDTH/2):
+                    return s.position_volley
+                else :
+                    return s.to_ball     
+        
